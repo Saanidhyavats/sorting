8f14786692c155d48cc87752ff6e40d62cf9e9dd
Nishant-Ingle <30694286+Nishant-Ingle@users.noreply.github.com>
2019-12-31 19:12:10 +0530

    Update sort.c
    Indentation and formatting done

--- sort.c
+++ sort.c
@@ -1,25 +1,32 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-int main()
-{ int a,b,c,d,e,min;
+int main() { 
+  int a,b,c,d,e,min;
+  int i[a];
   printf("Enter the number of data you want to enter");
-  scanf("%d",&a);
-   int i[a];
-  for(b=0;b<a;b++)
-  scanf("%d",&i[b]);
-  min=i[0];
-  for(e=0;e<a;e++)
- {for(b=0;b<a;b++)
-  {if(min>i[b])
-   {c=min;
-    min=i[b];
-   i[b]=c;}}
-   d=i[b];
-   i[b]=min;
-   min=d;}
-  for(b=0;b<a;b++)
-  printf("%d",i[b]);
+  scanf("%d", &a);
+  
+  for(b = 0; b < a; b++)
+    scanf("%d", &i[b]);
+  
+  min = i[0];
+  
+  for(e = 0; e < a; e++){
+    for(b = 0; b < a; b++){
+      if(min > i[b]){
+        c = min;
+        min = i[b];
+        i[b] = c;
+      }
+    }
+    d=i[b];
+    i[b]=min;
+    min=d;
   }
+  
+  for(b = 0; b < a; b++)
+    printf("%d",i[b]);
+}
    
   
